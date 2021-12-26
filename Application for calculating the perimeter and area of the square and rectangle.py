84228e5c84f98073007f57312147eca394715b8e
metameta <metaillusion000@gmail.com>
2021-12-26 18:34:32 +0600

    Create Application for calculating the perimeter and area of the square and rectangle.py

---
+++ Application for calculating the perimeter and area of the square and rectangle.py
@@ -0,0 +1,12 @@
+toknowlenght = int(input('Введите длину стороны квадрата: '))
+perimetr = toknowlenght * 4
+square = toknowlenght ** 2
+print('Периметр:', perimetr)
+print('Площадь:', square)
+
+toknowlength_1 = int(input('Введите длину прямоугольника: '))
+toknowwidth_1 = int(input('Введите ширину прямоугольника: '))
+perimetr_1 = (toknowlength_1 + toknowwidth_1)*2
+square_1 = toknowlength_1 * toknowwidth_1
+print("Периметр: ", perimetr_1)
+print("Площадь: ", square_1)
